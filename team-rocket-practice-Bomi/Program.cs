f46742210cd66c597fbc23b3837424ae7bd86e47
Bomi <asd24661413@gmail.com>
2025-12-08 13:18:55 +0800

    加入專案檔案。

---
+++ team-rocket-practice-Bomi/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace team_rocket_practice_Bomi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
