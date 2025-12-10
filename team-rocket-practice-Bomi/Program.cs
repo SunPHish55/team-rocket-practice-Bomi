f83d9c7e8f10626116e96c5bfcc19e75530168c4
Bomi <asd24661413@gmail.com>
2025-12-10 14:54:19 +0800

    update : finish extra practice 4

--- team-rocket-practice-Bomi/Program.cs
+++ team-rocket-practice-Bomi/Program.cs
@@ -613,6 +613,94 @@ namespace team_rocket_practice_Bomi
             //}
             //Console.WriteLine($"由大到小排列為：{n1}, {n2}, {n3}");
 
+
+
+
+
+
+
+            ////補充習題4. 獎金計算
+            //double profit, bonus;
+            //Console.Write("請輸入利潤：");
+            //profit = Convert.ToDouble(Console.ReadLine());
+            //if (profit <= 0)
+            //{
+            //    Console.WriteLine("公司要倒啦！");
+            //    return;
+            //}
+
+            //if (profit > 0 && profit <= 100000)
+            //{
+            //    bonus = profit * 0.1;
+            //}
+            //else if (profit > 100000 && profit <= 200000)
+            //{
+            //    bonus = 100000 * 0.1 + (profit - 100000) * 0.075;
+            //}
+            //else if (profit > 200000 && profit <= 400000)
+            //{
+            //    bonus = 100000 * 0.1 + 100000 * 0.075 + (profit - 200000) * 0.05;
+            //}
+            //else if (profit > 400000 && profit <= 600000)
+            //{
+            //    bonus = 100000 * 0.1 + 100000 * 0.075 + 200000 * 0.05 + (profit - 400000) * 0.03;
+            //}
+            //else if (profit > 600000 && profit <= 1000000)
+            //{
+            //    bonus = 100000 * 0.1 + 100000 * 0.075 + 200000 * 0.05 + 200000 * 0.03 + (profit - 600000) * 0.015;
+            //}
+            //else
+            //{
+            //    bonus = 100000 * 0.1 + 100000 * 0.075 + 200000 * 0.05 + 200000 * 0.03 + 400000 * 0.015 + (profit - 1000000) * 0.01;
+            //}
+            //Console.WriteLine("本月獎金為：" + bonus);
+
+
+
+
+
+            ////補充習題4. 獎金計算，優化版
+            //double profit, bonus = 0;
+            //Console.Write("請輸入利潤：");
+            //profit = Convert.ToDouble(Console.ReadLine());
+            //if (profit <= 0)
+            //{
+            //    Console.WriteLine("公司要倒啦！");
+            //    return;
+            //}
+
+            //double remaining = profit;
+            //for (int i = 0; i < 6; i++)
+            //{
+            //    double limit = 0, rate = 0;
+            //    switch (i)
+            //    {
+            //        case 0:
+            //            limit = 1000000; rate = 0.01;
+            //            break;
+            //        case 1:
+            //            limit = 600000; rate = 0.015;
+            //            break;
+            //        case 2:
+            //            limit = 400000; rate = 0.03;
+            //            break;
+            //        case 3:
+            //            limit = 200000; rate = 0.05;
+            //            break;
+            //        case 4:
+            //            limit = 100000; rate = 0.075;
+            //            break;
+            //        case 5:
+            //            limit = 0; rate = 0.1;
+            //            break;
+            //    }
+            //    if (remaining > limit)
+            //    {
+            //        bonus += (remaining - limit) * rate;
+            //        remaining = limit;
+            //    }
+            //}
+            //Console.WriteLine("本月獎金為：" + bonus);
         }
     }
 }
